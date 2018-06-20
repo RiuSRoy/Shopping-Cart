9a86618c967fa1f9f99c3adbb7d5e6d949dd237f
Aishwariya Singha Roy <iamash20@gmail.com>
2018-06-20 12:27:50 +0530

    Populate this with your own configurations

--- config/keys.js
+++ config/keys.js
@@ -2,13 +2,13 @@
 
 module.exports = {
     google: {
-        CLIENT_ID : '1046826826756-2jecv7dctl1p7q182pqhoc4806ee8guo.apps.googleusercontent.com',
-        CLIENT_SECRET : 'be8CtZiew3n9Z8qSZWO2-eDt'
+        CLIENT_ID : 'YOUR_GOOGLE_CLIENT_ID',
+        CLIENT_SECRET : 'YOUR_GOOGLE_CLIENT_SECRET'
     },
     mongodb : {
         MONGODB_URI : 'mongodb://localhost:27017/shopping'
     },
     session : {
-        SECRET_KEY : 'aindrila'
+        SECRET_KEY : 'YOUR_SECRET_KEY'
     }
 }
